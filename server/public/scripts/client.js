3e1a9520886ff17802fd01b75169aeb021a1b475
Marta Jopp <martapeterson@gmail.com>
2018-05-28 22:48:10 -0500

    angular working files connected

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,12 +1,13 @@
+console.log('sourced')
 var myApp = angular.module('myApp', ['ngRoute']);
 
-myApp.config(function ($routeProvider, $locationProvider) {
-    $locationProvider.hashPrefix('');
-    console.log('myApp -- config')
-    $routeProvider
-        // .when('/home', {
-        //     templateUrl: '/views/templates/home.html',
-        //     controller: 'LoginController as lc',
-        // })
-    })
+// myApp.config(function ($routeProvider) {
+//     $routeProvider.when('/home', {
+//         templateUrl: 'index.html',
+//         controller: 'ListingsController as listing'
+//     }).when('/rentals', {
+//         templateUrl: 'templates/rentals.html',
+//         controller: 'RentalsController as rental'
+//     })
+// })
 
--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
 var port = process.env.PORT || 5000;
+var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
-app.use(express.static('./server/public'));
+// static files
+app.use(express.static('server/public'));
 
 app.listen(port, function () {
     console.log('Listening on port:', port);
